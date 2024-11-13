e6e48b73c00c9df52a2d8b277880601e72fb7ce2
ela6993 <alel130107@gmail.com>
2024-11-13 21:22:28 +0600

    the third commit

--- react-homework/src/App.js
+++ react-homework/src/App.js
@@ -1,24 +1,13 @@
 import React, { useState } from 'react'
+import Counter from "./componets/Counter"
 
 function App() {
-  const [likes, setLikes] = useState(0)
-  const[value, setValue] = useState('Hello World!')
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
 
   return (
-    <div className = 'App'>
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input value = {value} onChange={event => setValue(event.target.value)}></input>
-      <button onClick={increment}>Increment</button><br></br>
-      <button onClick={decrement}>Decrement</button>
+    <div>
+      <Counter/>
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
